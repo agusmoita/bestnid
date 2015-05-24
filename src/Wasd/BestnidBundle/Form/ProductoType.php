b89fbad4e929d5102f9c3045e06df5284b579ce1
agusmoita <a94moita@outlook.com>
2015-05-24 18:37:01 -0300

    Alta de Producto

---
+++ src/Wasd/BestnidBundle/Form/ProductoType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Wasd\BestnidBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProductoType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titulo')
+            ->add('descripcion','textarea', array(
+                'label' => 'Descripción'
+            ))
+            ->add('vencimiento')
+            ->add('foto','file', array(
+                'label' => 'Foto'
+            ))
+            ->add('categoria')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Wasd\BestnidBundle\Entity\Producto'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'wasd_bestnidbundle_producto';
+    }
+}
